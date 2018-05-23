e2e42bdfceb126694f326645c81c2920dd93958e
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-05-23 20:25:17 +0200

    move coroutines

--- include/agents/coroutine.h
+++ include/agents/coroutine.h
@@ -1,8 +1,58 @@
 #ifndef AGENTS_COROUTINE_H_
 #define AGENTS_COROUTINE_H_
 
-#define PULL_BEGIN() static int state=0; switch(state) { case 0:
-#define PULL_CONTINUE(curr, val) do { state=curr; return val; case val:;} while(0)
-#define PULL_FINISH() }
+#define PULL_BEGIN(state)  \
+    static agent_t agent = {};\
+    agent.current_state = state; \
+    switch(agent.current_state) { case 0:
 
-#endif
+#define PULL_CONTINUE(state) \
+        do { \
+            agent.current_state=state; \
+            agent.current_error=PULL_SUCCESS; \
+            agent.required_action=CONTINUE; \
+            return agent; \
+            case agent.current_state:; \
+        } while(0)
+
+#define PULL_SEND(state) \
+        do { \
+            agent.current_state=state; \
+            agent.current_error=PULL_SUCCESS; \
+            agent.required_action=SEND; \
+            return agent; \
+            case agent.current_state:; \
+        } while(0);
+
+#define PULL_CONTINUE(state) \
+        do { \
+            agent.current_state=state; \
+            agent.current_error=PULL_SUCESS; \
+            agent.required_action=CONTINUE; \
+            return agent; \
+            case agent.current_state:; \
+        } while(0)
+
+#define PULL_RECOVER(state, error) \
+        do { \
+            agent.current_state=state; \
+            agent.current_error=error; \
+            agent.current_action=RECOVER; \
+            return agent; \
+            case agent.current_state:; \
+        } while (0);
+
+#define PULL_FAILURE(error) \
+        do { \
+            agent.current_error=error; \
+            agent.current_action=failure; \
+            return agent; \
+        } while (0);
+
+#define PULL_FINISH(state) } \
+    agent.current_state=state; \
+    agent.current_error=PULL_SUCESS; \
+    agent.current_action=APPLY; \
+    return agent; \
+
+#endif /* AGENTS_COROUTINE_H_ */
--- include/agents/update.h
+++ include/agents/update.h
@@ -4,60 +4,6 @@
 #include <common/libpull.h>
 #include <agents/coroutines.h>
 
-#define PULL_BEGIN(state)  \
-        static agent_t agent = {};\
-        agent.current_state = state; \
-        switch(agent.current_state) { case 0:
-
-#define PULL_CONTINUE(state) \
-        do { \
-            agent.current_state=state; \
-            agent.current_error=PULL_SUCCESS; \
-            agent.required_action=CONTINUE; \
-            return agent; \
-            case agent.current_state:; \
-        } while(0)
-
-#define PULL_SEND(state) \
-        do { \
-            agent.current_state=state; \
-            agent.current_error=PULL_SUCCESS; \
-            agent.required_action=SEND; \
-            return agent; \
-            case agent.current_state:; \
-        } while(0);
-
-#define PULL_CONTINUE(state) \
-        do { \
-            agent.current_state=state; \
-            agent.current_error=PULL_SUCESS; \
-            agent.required_action=CONTINUE; \
-            return agent; \
-            case agent.current_state:; \
-        } while(0)
-
-#define PULL_RECOVER(state, error) \
-        do { \
-            agent.current_state=state; \
-            agent.current_error=error; \
-            agent.current_action=RECOVER; \
-            return agent; \
-            case agent.current_state:; \
-        } while (0);
-
-#define PULL_FAILURE(error) \
-        do { \
-            agent.current_error=error; \
-            agent.current_action=failure; \
-            return agent; \
-        } while (0);
-
-#define PULL_FINISH(state) } \
-        agent.current_state=state; \
-        agent.current_error=PULL_SUCESS; \
-        agent.current_action=APPLY; \
-        return agent; \
-
 /* This states will be used by the update agent coroutines */
 typedef enum agent_state_t {
     STATE_INIT = 0,
