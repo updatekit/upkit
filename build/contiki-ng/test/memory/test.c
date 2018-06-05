7760004a41d61c6532a225ec89803445c48cd179
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-06-05 22:05:25 +0200

    fix: errors in test

--- build/contiki-ng/test/memory/test.c
+++ build/contiki-ng/test/memory/test.c
@@ -11,7 +11,7 @@
         printf("pass: "__VA_ARGS__); \
     }
 
-#define BUFFER_SIZE 200/sizeof(int)
+#define BUFFER_SIZE 200
 
 PROCESS(memory_test, "Test memory");
 AUTOSTART_PROCESSES(&memory_test);
@@ -23,27 +23,30 @@ PROCESS_THREAD(memory_test, ev, data) {
     int memory_test[BUFFER_SIZE];
     for (i=0; i<BUFFER_SIZE; i++) {
         memory_buffer[i] = i;
+        memory_test[i] = 0;
     }
     mem_object_t obj;
     pull_error err = memory_open(&obj, OBJ_2, WRITE_ALL);
     CHECK(err, "Error opening memory object 2 %s\n", err_as_str(err));
-    int not_correctly_ereased = 0;
-    int value = memory_read(&obj, memory_test, BUFFER_SIZE, 0);
-    CHECK(value != BUFFER_SIZE, "Error reading ereased memory\n");
-    value = memory_write(&obj, memory_buffer, BUFFER_SIZE, 0);
-    CHECK(value != BUFFER_SIZE, "Error writing buffer");
+    int value = memory_read(&obj, memory_test, sizeof(memory_test), 0);
+    CHECK(value != sizeof(memory_test), "Error reading ereased memory\n");
+
+    value = memory_write(&obj, memory_buffer, sizeof(memory_buffer), 0);
+    CHECK(value != sizeof(memory_buffer), "Error writing buffer");
     err = memory_close(&obj);
     CHECK(err, "Error closing object %s\n", err_as_str(err));
     err = memory_open(&obj, OBJ_2, READ_ONLY);
     CHECK(err, "Error reopening in reading mode %s\n", err_as_str(err));
-    value = memory_read(&obj, memory_test, BUFFER_SIZE, 0);
-    CHECK(value != BUFFER_SIZE, "Error reading memory %s\n", err_as_str(err));
-    int res = memcmp(memory_buffer, memory_test, BUFFER_SIZE);
+    value = memory_read(&obj, memory_test, sizeof(memory_test), 0);
+    CHECK(value != sizeof(memory_test), "Error reading memory %s\n", err_as_str(err));
+    int res = memcmp(memory_buffer, memory_test, sizeof(memory_test));
     CHECK(res != 0, "The readed data is different from the writted one\n");
     printf("Success\n");
 final:
-    for (i=0; i<30; i++) {
-        printf("%0x, ", memory_test[i]);
+    for (i=0; i<BUFFER_SIZE; i++) {
+        if (memory_buffer[i] != memory_test[i]) {
+            printf("%0x != %0x, ", memory_buffer[i], memory_test[i]);
+        }
     }
     while (1) {
        // NOP 
