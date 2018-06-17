3f4d27b86cb7493406bf1d1dc4d24599b1e1cfbc
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-06-17 21:33:54 +0200

    fix: avoid symbol collisions

--- include/libpull/memory/flash_interface.h
+++ include/libpull/memory/flash_interface.h
@@ -6,24 +6,24 @@
 #if MEMORY_FLASH_IMPL == 1
 
 // All these functions should return < 0 on error and >= 0 in case of success
-typedef int (*flash_open) (void);
-typedef int (*flash_erase) (address_t offset, size_t page_size);
-typedef int (*flash_program) (const uint8_t* buffer, address_t offset, size_t size);
-typedef int (*flash_read) (uint8_t* buffer, address_t offset, size_t size);
-typedef int (*flash_close) (void);
-typedef void (*watchdog_rst) (void);
+typedef int (*_flash_open) (void);
+typedef int (*_flash_erase) (address_t offset, size_t page_size);
+typedef int (*_flash_program) (const uint8_t* buffer, address_t offset, size_t size);
+typedef int (*_flash_read) (uint8_t* buffer, address_t offset, size_t size);
+typedef int (*_flash_close) (void);
+typedef void (*_watchdog_rst) (void);
 
 uint32_t get_start_offset(mem_id_t id);
 uint32_t get_end_offset(mem_id_t id);
 
 typedef struct flash_descr_t {
     size_t page_size;
-    flash_open open;
-    flash_erase erase;
-    flash_program program;
-    flash_read read;
-    flash_close close;
-    watchdog_rst rst;
+    _flash_open open;
+    _flash_erase erase;
+    _flash_program program;
+    _flash_read read;
+    _flash_close close;
+    _watchdog_rst rst;
 } flash_descr_t;
 
 typedef struct mem_object_t {
