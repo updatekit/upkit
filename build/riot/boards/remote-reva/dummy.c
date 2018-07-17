c05e94b4bdfaef11be3c07de17971d801783c7cf
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-07-17 18:10:46 +0200

    feat: add CI build script for RIOT

---
+++ build/riot/boards/remote-reva/dummy.c
@@ -0,0 +1,5 @@
+// This file is necessary to trigger the Makefile build.
+// In fact, a RIOT module must compile at least one file.
+// However, in this module we only want to compile the 
+// linker script
+void dummy(void) {}
