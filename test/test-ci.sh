6c015110cf206e742ec8c9ebac7bab7466912cc6
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-07-21 18:05:22 +0200

    fix: remove coveralls push from travis script

--- test/test-ci.sh
+++ test/test-ci.sh
@@ -7,3 +7,5 @@ make assets
 ./utils/server/server -f assets/slot_d.bin >/dev/null 2>&1 &
 make clean # This is needed since the library has been build before without coverage support
 make check
+
+coveralls -e test -e ext -e /usr -e utils -e build -i src/core -i src/agents -b test -r . --gcov-options '\-lp' \
