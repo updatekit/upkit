8b4529875e0cafa88df4fadf9d49b308c80ca3a6
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-07-21 17:50:59 +0200

    disable temporarely cryptoauthlib and prepare for dependencies caching

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -e
 
-# Fetch external dependencies
-if [ -d "ext" ]; then
-    rm -rf ext
-fi
-mkdir ext
-
 clone() {
     echo "Cloning $1..."
     git clone --quiet --progress --recursive \
@@ -16,46 +10,51 @@ clone() {
     echo "Cloning $1...done"
 }
 
-# Clone the repositories
-clone libcoap https://github.com/obgm/libcoap.git "16685d7"
-clone tinydtls https://git.eclipse.org/r/tinydtls/org.eclipse.tinydtls
-clone tinycrypt https://github.com/01org/tinycrypt.git
-clone unity https://github.com/ThrowTheSwitch/Unity.git
-clone cryptoauthlib https://github.com/MicrochipTech/cryptoauthlib.git
+# if the ext dir is already present then assume it is not modified and correctly
+# cached
+if [ ! -d "ext" ]; then
+    mkdir ext
+    clone libcoap https://github.com/obgm/libcoap.git "16685d7"
+    clone tinydtls https://git.eclipse.org/r/tinydtls/org.eclipse.tinydtls
+    clone tinycrypt https://github.com/01org/tinycrypt.git
+    clone unity https://github.com/ThrowTheSwitch/Unity.git
+    #clone cryptoauthlib https://github.com/MicrochipTech/cryptoauthlib.git
 
-# Patch the repositories
-PATCHDIR=patches
-for dir in $(cd $PATCHDIR && find * -type d -print); do
-    echo "Dir is $dir"
-    for f in $(find "$PATCHDIR/$dir" -maxdepth 1 -name "*.patch"| sort); do
-        patch=$PWD/$f
-        echo "Applying patch: $patch"
-        (cd ext/$dir && git am --ignore-whitespace --ignore-space-change $patch)
+    # Patch the repositories
+    PATCHDIR=patches
+    for dir in $(cd $PATCHDIR && find * -type d -print); do
+        echo "Dir is $dir"
+        for f in $(find "$PATCHDIR/$dir" -maxdepth 1 -name "*.patch"| sort); do
+            patch=$PWD/$f
+            echo "Applying patch: $patch"
+            (cd ext/$dir && git am --ignore-whitespace --ignore-space-change $patch)
+        done
     done
-done
 
-# Build libcoap
-(echo "Build libcoap..."
-cd ext/libcoap
-./autogen.sh
-touch ext/tinydtls/install.sh
-./configure --with-tinydtls --disable-shared --disable-examples
-make
-echo "Build libcoap...done")
+    # Build libcoap
+    (echo "Build libcoap..."
+    cd ext/libcoap
+    ./autogen.sh
+    touch ext/tinydtls/install.sh
+    ./configure --with-tinydtls --disable-shared --disable-examples
+    make
+    echo "Build libcoap...done")
 
-# Build tinydtls
-(echo "Build tinydtls..."
-cd ext/tinydtls
-touch install.sh
-POSIX_OPTIMIZATIONS="-Os -ffunction-sections -fdata-sections -Wl,--gc-sections"
-autoreconf -i --force
-./configure --without-debug
-make libtinydtls.a CFLAGS="$POSIX_OPTIMIZATIONS"
-echo "Build tinydtls...done")
+    # Build tinydtls
+    (echo "Build tinydtls..."
+    cd ext/tinydtls
+    touch install.sh
+    POSIX_OPTIMIZATIONS="-Os -ffunction-sections -fdata-sections -Wl,--gc-sections"
+    autoreconf -i --force
+    ./configure --without-debug
+    make libtinydtls.a CFLAGS="$POSIX_OPTIMIZATIONS"
+    echo "Build tinydtls...done")
 
-# Build tinycrypt
-echo "Build tinycrypt..."
-(cd ext/tinycrypt && make ENABLE_TESTS=false)
-echo "Build tinycrypt...done"
+    # Build tinycrypt
+    echo "Build tinycrypt..."
+    (cd ext/tinycrypt && make ENABLE_TESTS=false)
+    echo "Build tinycrypt...done"
+
+fi
 
 autoreconf -i
