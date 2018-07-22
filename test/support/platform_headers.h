74b944bd4f4e2e26b4ea7b7298ad47b845ece761
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-07-22 17:32:45 +0200

    move async header and fix build

--- test/support/platform_headers.h
+++ test/support/platform_headers.h
@@ -2,7 +2,7 @@
 #define PLATFORM_HEADERS_H_
 
 #include "src/platform/posix/memory_posix.h"
-#include "src/network/coap/libcoap/connection_libcoap.h"
-#include "src/network/coap/libcoap/async_libcoap.h"
+#include <libpull_network/coap/connection_libcoap.h>
+#include <libpull_network/coap/async_libcoap.h>
 
 #endif /* PLATFORM_HEADERS_H_ */
