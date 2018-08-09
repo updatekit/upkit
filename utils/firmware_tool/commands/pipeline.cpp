0b5602a73498b197a8ffb1eea4af5a5e11147957
Antonio Langiu <antonio.langiu@studenti.polito.it>
2018-08-09 11:45:27 +0200

    feat: add decompression routine

--- utils/firmware_tool/commands/pipeline.cpp
+++ utils/firmware_tool/commands/pipeline.cpp
@@ -1,32 +1,213 @@
 #include <iostream>
-#include <string.h>
-#include "commands.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <libpull/memory.h>
 #include <libpull/security.h>
 #include <libpull/pipeline.h>
 
+#include "commands.hpp"
+
 IMPLEMENT(pipeline)
 
+/*****************************************************************************/
+/********************** Start LZSS encoder and decoder ***********************/
+/*****************************************************************************/
+
+/* The LZSS encoder and decoder and inspired from the
+ * Haruhiko Okumura version released to the public domain */
+#define EI 10  /* typically 10..13 */
+#define EJ  4  /* typically 4..5 */
+#define P   1  /* If match length <= P then output one character */
+#define N (1 << EI)  /* buffer size */
+#define F ((1 << EJ) + 1)  /* lookahead buffer size */
+int bit_buffer = 0, bit_mask = 128;
+unsigned long codecount = 0, textcount = 0;
+FILE *infile, *outfile;
+
+void error(void)
+{
+    printf("Output error\n");  exit(1);
+}
+
+void putbit1(void)
+{
+    bit_buffer |= bit_mask;
+    if ((bit_mask >>= 1) == 0) {
+        if (fputc(bit_buffer, outfile) == EOF) error();
+        bit_buffer = 0;  bit_mask = 128;  codecount++;
+    }
+}
+
+void putbit0(void)
+{
+    if ((bit_mask >>= 1) == 0) {
+        if (fputc(bit_buffer, outfile) == EOF) error();
+        bit_buffer = 0;  bit_mask = 128;  codecount++;
+    }
+}
+
+void flush_bit_buffer(void)
+{
+    if (bit_mask != 128) {
+        if (fputc(bit_buffer, outfile) == EOF) error();
+        codecount++;
+    }
+}
+
+void output1(int c)
+{
+    int mask;
+
+    putbit1();
+    mask = 256;
+    while (mask >>= 1) {
+        if (c & mask) putbit1();
+        else putbit0();
+    }
+}
+
+void output2(int x, int y)
+{
+    int mask;
+
+    putbit0();
+    mask = N;
+    while (mask >>= 1) {
+        if (x & mask) putbit1();
+        else putbit0();
+    }
+    mask = (1 << EJ);
+    while (mask >>= 1) {
+        if (y & mask) putbit1();
+        else putbit0();
+    }
+}
+
+/* The compress functions needs to get the
+ * following values from the context:
+ * 
+ *
+ */
 int pipeline_compress_command(Context ctx) {
-    std::cout << 1 << std::endl;
+    int i, j, f1, x, y, r, s, bufferend, c;
+    static unsigned char buffer[N * 2];
+
+    for (i = 0; i < N - F; i++) buffer[i] = ' ';
+    for (i = N - F; i < N * 2; i++) {
+        if ((c = fgetc(infile)) == EOF) break;
+        buffer[i] = c;  textcount++;
+    }
+    bufferend = i;  r = N - F;  s = 0;
+    while (r < bufferend) {
+        f1 = (F <= bufferend - r) ? F : bufferend - r;
+        x = 0;  y = 1;  c = buffer[r];
+        for (i = r - 1; i >= s; i--)
+            if (buffer[i] == c) {
+                for (j = 1; j < f1; j++)
+                    if (buffer[i + j] != buffer[r + j]) break;
+                if (j > y) {
+                    x = i;  y = j;
+                }
+            }
+        if (y <= P) {  y = 1;  output1(c);  }
+        else output2(x & (N - 1), y - 2);
+        r += y;  s += y;
+        if (r >= N * 2 - F) {
+            for (i = 0; i < N; i++) buffer[i] = buffer[i + N];
+            bufferend -= N;  r -= N;  s -= N;
+            while (bufferend < N * 2) {
+                if ((c = fgetc(infile)) == EOF) break;
+                buffer[bufferend++] = c;  textcount++;
+            }
+        }
+    }
+    flush_bit_buffer();
+    printf("text:  %ld bytes\n", textcount);
+    printf("code:  %ld bytes (%ld%%)\n",
+            codecount, (codecount * 100) / textcount);
     return 0;
 }
 
-int pipeline_decompress_command(Context ctx) {
-    std::cout << 2 << std::endl;
-    return 0;
-    
+static int getbit(std::ifstream &input, int n) {
+    int i, x;
+    static int buf, mask = 0;
+    char c;
+    x = 0;
+    for (i = 0; i < n; i++) {
+        if (mask == 0) {
+            if (input.eof()) {
+                return EOF;
+            }
+            input.get(c);
+            buf = c;
+            mask = 128;
+        }
+        x <<= 1;
+        if (buf & mask) x++;
+        mask >>= 1;
+    }
+    return x;
 }
+
+/* This function receives a compressed file as input and
+ * decompresses it to another file. 
+ * The function needs to get the following parameters from
+ * the context:
+ * -b (binary_file): the compressed input file
+ * -f (output_file): the file where to store the decompressed version
+ */
+int pipeline_decompress_command(Context ctx) {
+    // ctx->binary_file is the input file name
+    // ctx->out_file is the output file name
+    int i, j, k, r, c;
+    static unsigned char buffer[N * 2];
+
+    // 0. Open the input file
+    std::ifstream input(ctx.get_binary_file());
+    if (! input.is_open()) {
+        std::cerr << "Error opening input file\n" << std::endl;
+        return EXIT_FAILURE;
+    }
+    // 1. Open the output file
+    std::ofstream output(ctx.get_out_file());
+    if (! output.is_open()) {
+        std::cerr << "Error opening output file\n" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // 2. Start the algorithm
+    for (i = 0; i < N - F; i++) buffer[i] = ' ';
+    r = N - F;
+    while ((c = getbit(input, 1)) != EOF) {
+        if (c) {
+            if ((c = getbit(input, 8)) == EOF) break;
+            output.put(c);
+            buffer[r++] = c;  r &= (N - 1);
+        } else {
+            if ((i = getbit(input, EI)) == EOF) break;
+            if ((j = getbit(input, EJ)) == EOF) break;
+            for (k = 0; k <= j + 1; k++) {
+                c = buffer[(i + k) & (N - 1)];
+                output.put(c);
+                buffer[r++] = c;  r &= (N - 1);
+            }
+        }
+    }
+    return 0;
+}
+
+/*****************************************************************************/
+/************************ Start bsdiff and bspatch ***************************/
+/*****************************************************************************/
 
 int pipeline_diff_command(Context ctx) {
-    std::cout << 3 << std::endl;
     return 0;
-    
 }
 
 int pipeline_patch_command(Context ctx) {
-    std::cout << 4 << std::endl;
     return 0;
-    
 }
